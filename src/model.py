064451e908bcd9690be29844941e67be5b0a2f21
Jason Chen <kbtpodifo@gmail.com>
2018-05-12 19:23:31 -0400

    Encapsulate interfacing with FaceNet in a class

--- src/model.py
+++ src/model.py
@@ -3,26 +3,27 @@ import numpy as np
 import tensorflow as tf
 from .facenet.src import facenet
 
-def load_model():
-    model_path = os.path.join(
-        os.path.dirname(__file__),
-        "..",
-        "saved_models/model_vggface2"
-    )
-    facenet.load_model(model_path)
+class FaceNet():
+    def __init__(self, sess):
+        self.sess = sess
+        model_path = os.path.join(
+            os.path.dirname(__file__),
+            "..",
+            "saved_models/model_vggface2"
+        )
+        facenet.load_model(model_path)
 
-def get_embeddings(img):
-    sess = tf.get_default_session()
-    graph = sess.graph
+    def get_embeddings(self, img):
+        graph = self.sess.graph
 
-    # Convert img shape (160, 160, 3) to (1, 160, 160, 3)
-    img = np.expand_dims(img, axis=0)
+        # Convert img shape (160, 160, 3) to (1, 160, 160, 3)
+        img = np.expand_dims(img, axis=0)
 
-    embeddings = graph.get_tensor_by_name("embeddings:0")
-    input_images = graph.get_tensor_by_name("input:0")
-    input_phase_train = graph.get_tensor_by_name("phase_train:0")
+        embeddings = graph.get_tensor_by_name("embeddings:0")
+        input_images = graph.get_tensor_by_name("input:0")
+        input_phase_train = graph.get_tensor_by_name("phase_train:0")
 
-    return sess.run(embeddings, feed_dict={
-        input_images: img,
-        input_phase_train: False
-    })
+        return self.sess.run(embeddings, feed_dict={
+            input_images: img,
+            input_phase_train: False
+        })
