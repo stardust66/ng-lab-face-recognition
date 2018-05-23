eb05d9d0aed123489903e2ef32ea8e6d2052a398
Jason Chen <kbtpodifo@gmail.com>
2018-05-23 19:53:15 -0400

    Implement cosine similarity metric in utils
    Also fix facenet import and raising errors

--- src/utils.py
+++ src/utils.py
@@ -1,31 +1,46 @@
 from contextlib import contextmanager
 import cv2
 import numpy as np
-from . import facenet
+from .facenet.src import facenet
 
 def distance(embeddings1, embeddings2, metric="squared_l2"):
     """Calculates distance between embeddings
 
     Metric can be 'squared_l2' or 'cosine_similarity'
     """
-
     # In the facenet library, distance metric 0 is squared l2 norm while
     # distance metric 1 is cosine similarity.
-    if metric == "l2_norm":
+    if metric == "squared_l2":
         return facenet.distance(embeddings1, embeddings2, distance_metric=0)[0]
     elif metric == "cosine_similarity":
         return facenet.distance(embeddings1, embeddings2, distance_metric=1)[0]
     else:
-        raise ("metric must be either squared_l2 or cosine_similarity"
-               ", instead received '{}'.".format(metric))
+        raise ValueError("metric must be either squared_l2 or cosine_similarity"
+                         ", instead received '{}'.".format(metric))
 
-def distance_from_all(embeddings_database, embeddings):
-    diff = np.subtract(embeddings_database, embeddings)
-    norm = np.sum(np.square(diff), axis=-1)
-    return np.squeeze(norm)
+def distance_from_all(embeddings_database, embeddings, metric="squared_l2"):
+    """Calculates the distance between embedding and all embeddings
 
-def classify(embeddings_database, embeddings, threshold):
-    distance = distance_from_all(embeddings_database, embeddings)
+    Metric can be 'squared_l2' or 'cosine_similarity'. The implementation
+    is largely based on David Sandberg's facenet.py, just adapted to work
+    with an array of embedding vectors.
+    """
+    if metric == "squared_l2":
+        diff = np.subtract(embeddings_database, embeddings)
+        norm = np.sum(np.square(diff), axis=-1)
+        return np.squeeze(norm)
+    elif metric == "cosine_similarity":
+        dot = np.sum(np.multiply(embeddings_database, embeddings), axis=-1)
+        database_norm = np.linalg.norm(embeddings_database, axis=-1)
+        embeddings_norm = np.linalg.norm(embeddings)
+        cosine = dot / (database_norm * embeddings_norm)
+        return np.arccos(cosine) / np.pi
+    else:
+        raise ValueError("metric must be either squared_l2 or cosine_similarity"
+                         ", instead received '{}'.".format(metric))
+
+def classify(embeddings_database, embeddings, threshold, metric):
+    distance = distance_from_all(embeddings_database, embeddings, metric)
     min_distance = np.amin(distance)
 
     if min_distance > threshold:
