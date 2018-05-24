c0e38057ac7884ba178f954f0e8100ddc12dfd7b
Jason Chen <kbtpodifo@gmail.com>
2018-05-24 13:51:01 -0400

    Automatically cache downloaded audio files

--- src/audio.py
+++ src/audio.py
@@ -2,20 +2,27 @@ from gtts import gTTS
 import os
 import time
 
+def play_audio(filename):
+    os.system('mpg321 "{}"'.format(filename))
+
 class AudioGreeter():
     def __init__(self, save_path):
         self.save_path = save_path
         self.previous_time = 0
 
     def greet(self, name):
+        filename = "{}/greet {}.mp3".format(self.save_path, name)
+
         if not self.seconds_passed(10):
             return
 
-        tts = gTTS("Hello {}".format(name))
-        filename = "{}/greet.mp3".format(self.save_path)
+        if os.path.isfile(filename):
+            play_audio(filename)
+            return
 
+        tts = gTTS("Hello {}".format(name))
         tts.save(filename)
-        os.system("mpg321 {}".format(filename))
+        play_audio(filename)
 
     def seconds_passed(self, seconds):
         current_time = time.time()
