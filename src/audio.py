81a29e6baf180483b626819ebd8a8d30e2cf1cbf
Jason Chen <kbtpodifo@gmail.com>
2018-05-23 20:48:27 -0400

    Add audio greeter class using Google Speech API

---
+++ src/audio.py
@@ -0,0 +1,26 @@
+from gtts import gTTS
+import os
+import time
+
+class AudioGreeter():
+    def __init__(self, save_path):
+        self.save_path = save_path
+        self.previous_time = -30
+
+    def greet(self, name):
+        if not self.check_time():
+            return
+
+        tts = gTTS("Hello {}".format(name))
+        filename = "{}/greet.mp3".format(self.save_path)
+
+        tts.save(filename)
+        os.system("mpg321 {}".format(filename))
+
+    def check_time(self):
+        current_time = time.time()
+        if current_time - self.previous_time < 20:
+            return False
+        else:
+            self.previous_time = current_time
+            return True
